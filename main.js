e8c6effaff2214edf8dc545cd4171b4b7853972f
okhat1 <omarkhattab2@gmail.com>
2022-12-28 22:52:36 -0500

    added channel restriction and a leave command

--- main.js
+++ main.js
@@ -15,20 +15,25 @@ const client = new Discord.Client({
 const prefix = '-';
 
 client.once('ready', () => {
-    console.log('its botting time');
+  console.log('its botting time');
 });
 
 client.on('messageCreate', (message) => {
-    if(!message.content.startsWith(prefix) || message.author.bot) return;
-    console.log('hi');
-    const args = message.content.slice(prefix.length).split(" ");
-    const command = args.shift().toLowerCase();
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).split(" ");
+  const command = args.shift().toLowerCase();
 
-    if(command === 'q'){
-        playerCount++;
-        let count = "There are " + playerCount + " players in queue";
-        message.channel.send(count);
+  if (message.channelId == "1057545714986655845") {
+    if (command === 'q') {
+      playerCount++;
+      let count = "There are " + playerCount + " players in queue";
+      message.channel.send(count);
+    } else if (command === 'l') {
+      playerCount--;
+      let count = "There are " + playerCount + " players in queue";
+      message.channel.send(count);
     }
+  }
 });
 
 client.login('MTA1Nzc3NTE0ODE4MDE4OTI4NQ.G-RTPE.iisxWvvfDReNsI5PCPkPrDfCJqxCVb8-nl-OE4');
