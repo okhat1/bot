6faf7368f322704645f0f51527fb8bc89ac53c46
okhat1 <omarkhattab2@gmail.com>
2022-12-28 22:12:44 -0500

    Added node and main functionality

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
+let playerCount = 0;
 
-hehe bebebe wowowooo
+const Discord = require('discord.js');
+
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Discord.Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ]
+})
+
+const prefix = '-';
+
+client.once('ready', () => {
+    console.log('its botting time');
+});
+
+client.on('messageCreate', (message) => {
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+    console.log('hi');
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    if(command === 'q'){
+        playerCount++;
+        let count = "There are " + playerCount + " players in queue";
+        message.channel.send(count);
+    }
+});
+
+client.login('MTA1Nzc3NTE0ODE4MDE4OTI4NQ.G-RTPE.iisxWvvfDReNsI5PCPkPrDfCJqxCVb8-nl-OE4');
